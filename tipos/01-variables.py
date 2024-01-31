54326284ac35be386c2f7aff8eb56828ae405a8e
Nicolas <nicolasgfalabella@gmail.com>
2024-01-31 00:04:59 -0300

    String

---
+++ tipos/01-variables.py
@@ -0,0 +1,9 @@
+"""Curso de Python"""
+nombre_curso = "Ultimate Pyhon"
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NombreCurso = "Chanchito"
+print(nombre_curso, nombre1, NOMBRE_CURSO , NombreCurso)
+ALUMNOS  = 5000
+puntaje = 9.9
+pubilcado = True
---
+++ tipos/02-strings.py
@@ -0,0 +1,10 @@
+nombre_curso = "Ultimate python"
+descripcion_curso= """
+Ultimate python este curso contemplalos detalles para aprender este lenguaje de programacion
+"""
+
+print(len(nombre_curso))
+print(nombre_curso[0])
+print(nombre_curso[0:8])
+print(nombre_curso[9:])
+print(nombre_curso[:9])
