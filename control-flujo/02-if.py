7c67d03a4e4ee3a9152d0e3b5c89416e71eaf8ff
Nicolas <nicolasgfalabella@gmail.com>
2024-02-02 12:09:51 -0300

    iF

---
+++ control-flujo/02-if.py
@@ -0,0 +1,10 @@
+edad = 55
+if edad > 54:
+    print("Puede ver la Pelicula con descuento")
+elif edad > 17 :
+    print("Puede ver la pelicula")
+else:
+    print("No puede entrar")
+    print("Ve a otro lado")
+
+print("listo")
