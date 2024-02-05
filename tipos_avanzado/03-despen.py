77e537cf517c924cf2912f5fac4636a329ac028f
Nicolas <nicolasgfalabella@gmail.com>
2024-02-05 11:00:04 -0300

    Desepaquetado

---
+++ tipos_avanzado/03-despen.py
@@ -0,0 +1,8 @@
+numeros = [1,2,3,4,5,6,7,8,9]
+
+# primero = numeros[0]
+# segundo = numeros[1]
+# tercero = numeros[2]
+
+primero,segundo, *otros, penu, ultimo = numeros
+print(primero ,segundo,otros,penu, ultimo)
