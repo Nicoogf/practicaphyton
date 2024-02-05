78f448cc459988a7a3bd516f398ea8920923e62a
Nicolas <nicolasgfalabella@gmail.com>
2024-02-05 10:48:03 -0300

    Listas manipulacion

---
+++ tipos_avanzado/01-listas.py
@@ -0,0 +1,42 @@
+numero = [1,
+          2,
+          3
+          ]
+
+print( numero )
+
+letras = ["a" ,"b" ,"c"]
+palabras = ["Chanchito" , "Feliz" ,"Felipe" , "Alumnos"]   
+
+print(palabras)
+
+
+booleans = [ True , False ,False, True]
+print(booleans)
+
+
+matriz = [
+    ["Hola mundo"] ,
+    [0,1,2,3],
+    [True, False,False],
+    [0,12,3]
+]
+
+
+ceros = [ 0 , 1 ] * 10
+
+print(ceros)
+
+
+alfanumerico = numero + letras
+
+print(alfanumerico)
+
+
+rango= list(range(1, 11))
+print(rango)
+
+
+chars =list("Holamundo")
+
+print(chars)
