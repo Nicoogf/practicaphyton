738746532924379cd040e7cc4b9c9731061df691
Nicolas <nicolasgfalabella@gmail.com>
2024-02-06 11:04:30 -0300

    Tuplas

---
+++ tipos_avanzado/09-tupla.py
@@ -0,0 +1,15 @@
+numeros = ( 1,2,3 ) + (4,5,6) 
+
+print(numeros)
+
+punto = tuple([1 , 2])
+menosNumeros = numeros[:2]
+
+print(menosNumeros)
+
+primero,segundo,*otros = numeros
+
+print(primero, segundo, otros)
+
+for n in numeros :
+    print(n)
