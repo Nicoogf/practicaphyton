3e58ee558668afb7077614ed321b8a559c311740
Nicolas <nicolasgfalabella@gmail.com>
2024-02-06 11:14:33 -0300

    Set

---
+++ tipos_avanzado/10-set.py
@@ -0,0 +1,13 @@
+primer = {1, 1, 2, 3, 4, 4, }
+
+segundo = [3, 4, 5]
+
+segundo = set(segundo)
+
+print(primer | segundo)
+print(primer & segundo)
+print(primer - segundo)
+print( primer ^ segundo )
+
+if 5 in segundo:
+    print("hola mundo")
