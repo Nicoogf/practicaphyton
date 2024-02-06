dc0bc35cf96ad5dfc3e3d3134cb51189396b068d
Nicolas <nicolasgfalabella@gmail.com>
2024-02-06 14:12:22 -0300

    intro backend

---
+++ backend/type-hints.py
@@ -0,0 +1,17 @@
+"""Tipe hints"""
+
+my_string_varibale = "My String Variable"
+print(my_string_varibale)
+print(type(my_string_varibale))
+
+my_string_varibale = 5
+
+print(my_string_varibale)
+
+print(type(my_string_varibale))
+
+my_typed_variable: str = "String"
+my_string_varibale.
+print(my_typed_variable)
+print(type(my_typed_variable))
+
---
+++ tipos_avanzado/12-desempaquetado.py
@@ -0,0 +1,21 @@
+# lista = [1,2,3,4]
+
+# # print(lista)
+
+# # print(*lista)
+
+# lista2= [ 5,6]
+
+
+# combinada = ["hola",*lista,"mundo", *lista2 ,"chanchito"]
+
+# print(combinada)
+
+
+punto1 = {"x": 19 , "y" :"hola"}
+punto2 = {"y": 15}
+
+
+nuevoPunto= {**punto1 , **punto2 , "z" : "mundo"}
+
+print(nuevoPunto)
