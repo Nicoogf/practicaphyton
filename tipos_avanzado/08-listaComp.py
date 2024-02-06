5d9733333d952ff6b5707a142d55104021d3a068
Nicolas <nicolasgfalabella@gmail.com>
2024-02-06 10:50:27 -0300

    Comprension de listas

--- tipos_avanzado/08-listaComp.py
+++ tipos_avanzado/08-listaComp.py
@@ -13,8 +13,12 @@ usuarios = [
 # print(nombres)
 
 
-nombres = [usuario[0] for usuario in usuarios]
+# nombres = [usuario[0] for usuario in usuarios]
 
 # nombres = [usuario for usuario in usuarios if usuario[1] > 2]
-nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
-print(nombres)
+# nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
+
+nombres = list(map(lambda usuario: usuario[0], usuarios))
+menosNombres = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+menosNombresNuevo= list(map(lambda usuario: usuario[0] , menosNombres))
+print(menosNombresNuevo)
