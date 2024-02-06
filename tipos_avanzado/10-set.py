2cd6decc28c7383ee5ce7f4166cec0164b3bf09d
Nicolas <nicolasgfalabella@gmail.com>
2024-02-06 13:04:28 -0300

    diccionario de usuarios

--- tipos_avanzado/10-set.py
+++ tipos_avanzado/10-set.py
@@ -1,4 +1,4 @@
-primer = {1, 1, 2, 3, 4, 4, }
+primer = {1, 1, 2, 3, 4, 4}
 
 segundo = [3, 4, 5]
 
@@ -7,7 +7,7 @@ segundo = set(segundo)
 print(primer | segundo)
 print(primer & segundo)
 print(primer - segundo)
-print( primer ^ segundo )
+print(primer ^ segundo)
 
 if 5 in segundo:
     print("hola mundo")
