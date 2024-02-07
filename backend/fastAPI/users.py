d1974092a19d9fc537017acdde24ac60cd2d3fea
Nicolas <nicolasgfalabella@gmail.com>
2024-02-07 08:46:20 -0300

    Get de Usuarios

--- backend/fastAPI/users.py
+++ backend/fastAPI/users.py
@@ -7,11 +7,17 @@ app = FastAPI()
 
 # Entidad User
 
-class User():
+class User(BaseModel):
     name: str
     surname: str
     url: str
     age: int
+
+users_list= [
+    User(name="Brais", surname="Moure", url="https://moure.dev", age=35),
+    User(name="Moure", surname="Dev", url="https://mouredev.com", age=27),
+    User(name="Haakon", surname="Dahlberg", url="https://Haakon.com", age=20)
+]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -36,3 +42,7 @@ async def usersjson():
          },
 
     ]
+
+@app.get("/users")
+async def userclass():
+    return users_list
