72aa99361833435eb9ba3cf369c620fb9f8f0789
Nicolas <nicolasgfalabella@gmail.com>
2024-02-08 20:33:42 -0300

    Rutas Post

--- backend/fastAPI/users.py
+++ backend/fastAPI/users.py
@@ -7,17 +7,23 @@ app = FastAPI()
 
 # Entidad User
 
+
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list= [
-    User(name="Brais", surname="Moure", url="https://moure.dev", age=35),
-    User(name="Moure", surname="Dev", url="https://mouredev.com", age=27),
-    User(name="Haakon", surname="Dahlberg", url="https://Haakon.com", age=20)
+
+users_list = [
+    User(id=1, name="Brais", surname="Moure", url="https://moure.dev", age=35),
+    User(id=2, name="Moure", surname="Dev",
+         url="https://mouredev.com", age=27),
+    User(id=3, name="Haakon", surname="Dahlberg",
+         url="https://Haakon.com", age=20)
 ]
+
 
 @app.get("/usersjson")
 async def usersjson():
@@ -26,23 +32,50 @@ async def usersjson():
             "name": "Brais",
             "surname": "Moure",
             "url": "https:/moure.dev",
-            "age" : 35
+            "age": 35
         },
         {
             "name": "Moure",
             "surname": "Dev",
             "url": "https:/mouredev.com",
-            "age" : 27
+            "age": 27
         },
         {
-            "name": "Haakon" , 
-            "surname" :"Dahlberg" ,
+            "name": "Haakon",
+            "surname": "Dahlberg",
             "url": "https:/Haakon.com",
-            "age" : 20
-         },
+            "age": 20
+        },
 
     ]
 
+
 @app.get("/users")
-async def userclass():
+async def users():
     return users_list
+
+
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+
+@app.get("/user")
+async def user(id: int):
+    return search_user(id)
+
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "Usuario no encontrado"}
