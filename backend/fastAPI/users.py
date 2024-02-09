d3679a8e2ad4a11e2a982472b912aaf53acaaf93
Nicolas <nicolasgfalabella@gmail.com>
2024-02-09 10:19:25 -0300

    Avance sobre put y post

--- backend/fastAPI/users.py
+++ backend/fastAPI/users.py
@@ -72,6 +72,16 @@ async def user(user: User):
     else:
         users_list.append(user)
 
+@app.put("/user/")
+async def user(user :User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+        if not found:
+             return {"error": "Usuario no encontrado"}
+
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
