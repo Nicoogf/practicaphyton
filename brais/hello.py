d32e14270de53a3b9802007804a6626ba7d034dc
Nicolas <nicolasgfalabella@gmail.com>
2024-02-11 15:12:01 -0300

    Type

--- brais/hello.py
+++ brais/hello.py
@@ -1,2 +1,26 @@
-"""Hola mundo"""
-print("Hola mundo")
+
+print("Hola Mundo")
+print('Hola Python')
+
+"""
+Este comentario va a 
+poder comentar multiples 
+lineas
+"""
+
+
+'''
+Segunda prueba
+de comentario con
+multiples lineas
+'''
+
+
+print(type("Hola mundo"))
+print(type(65))
+print(type({"hola": "mundo"}))
+print(type([1,2,3]))
+print(type((1,2,3)))
+print(type(32.1))
+print(type(4j))
+print(type(True))
