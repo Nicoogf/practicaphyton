0e463ec2458fc03a1a00e6f277cfa623e1c88eb3
Nicolas <nicolasgfalabella@gmail.com>
2024-02-12 13:21:48 -0300

    Print vol 2

--- brais/01_variables.py
+++ brais/01_variables.py
@@ -2,3 +2,19 @@
 
 my_variable = "My string variable"
 print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+my_int_to_str_variable = str(my_int_variable)
+print(type(my_int_to_str_variable))
+
+print(my_bool_variable)
+print(my_variable, my_int_to_str_variable, my_bool_variable)
+
+print(type(print("hola")))
+
+# Len
+print(len(my_int_to_str_variable))
+print("El valor de la variables es : ", my_bool_variable)
