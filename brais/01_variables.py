83d09ad45750a74a8e323540e83e44dd15e3be6c
Nicolas <nicolasgfalabella@gmail.com>
2024-02-12 13:47:57 -0300

    Tipado

--- brais/01_variables.py
+++ brais/01_variables.py
@@ -18,3 +18,24 @@ print(type(print("hola")))
 # Len
 print(len(my_int_to_str_variable))
 print("El valor de la variables es : ", my_bool_variable)
+
+name, surname, alias, age = "Brais", "Moure", "Moure dev" , 43
+
+print( age , name , surname , "y mi alias es : " , alias )
+ """
+name = input("Ingresar nombre")
+edad = input("Ingresar edad")
+print(name , age)
+
+name = 35
+age = "Brais"
+print(name , age)
+ """
+
+
+
+#Forzamos el tipo
+address: str = "Mi Direccion"
+address = 32 
+
+print(type(address))
