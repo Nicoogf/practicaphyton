14807243bbc285f3fcbade60a6ee793a43052d77
Nicolas <nicolasgfalabella@gmail.com>
2024-02-15 11:39:34 -0300

    Clase de strings

--- brais/03_strings.py
+++ brais/03_strings.py
@@ -1,9 +1,4 @@
-my_string = "este string \ncon salto de linea"
+""" asd """
 
-print(my_string)
-
-nombre,edad,apellido = "nicolas" , 27 ,"falabella"
-
-print(" %s %s %s" %( nombre ,edad,apellido))
-
-nombre = "nicolas practica"
+my_string = "My String"
+my_other_string = 'My String'
