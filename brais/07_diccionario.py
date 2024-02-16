68e20e6d28fe2c5b1a493db8254f481fc6c11d8a
Nicolas <nicolasgfalabella@gmail.com>
2024-02-16 17:34:40 -0300

    Dics

--- brais/07_diccionario.py
+++ brais/07_diccionario.py
@@ -12,8 +12,31 @@ my_dict = {
         "Nombre" : "Brais" , 
         "Apellido" : "Moure" , 
         "Edad" : 35 , 
-        "Lenguajes": {"Python" , "Swift" , "Kotlin"}
+        "Lenguajes": {"Python" , "Swift" , "Kotlin"},
+        1 : 1.77
     }
 
 print( my_other_dict )
 print(my_dict)
+print(len(my_other_dict))
+print(len(my_dict))
+
+print(my_dict["Nombre"])
+
+my_dict["Nombre"] = "Adrian"
+
+print(my_dict["Nombre"])
+
+print(my_dict[1])
+
+my_dict["calle"] = "106"
+
+
+del my_dict["calle"]
+del my_dict["Apellido"]
+
+print(my_dict)
+
+print("Nombre" in my_dict)
+print("Brais" in my_dict)
+#print("Moure" in my_dict)
