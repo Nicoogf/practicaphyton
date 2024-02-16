40c518ab2d7a4e4d741e67fb59d7c7e3399508cf
Nicolas <nicolasgfalabella@gmail.com>
2024-02-15 21:16:19 -0300

    Avance list

--- brais/04_list.py
+++ brais/04_list.py
@@ -5,9 +5,39 @@ my_other_list = []
 
 print(len(my_list))
 
-my_list= [35,24,62,52,30,30,17]
+my_list : list= [35,24,62,52,30,30,17]
 my_other_list = [ 35 , 1.77 , "Brais" , "Moure"]
 
 print(type(my_list))
 print(my_list)
 print(my_list[0])
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[-1])
+print(my_list[-3])
+print(my_list[-4])
+print(my_list[-5])
+
+print(my_list.count(30))
+
+edad,altura,nombre,apellido = my_other_list
+
+print(my_list + my_other_list)
+
+
+
+
+print(my_list)
+print(type(my_list))
+
+my_list.append("hola")
+my_list.insert(2 , "Chau")
+
+
+
+
+
+
+
+print(my_list)
