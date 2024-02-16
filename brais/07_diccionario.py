b69c08c3af44bb4e28296fe031bb2a8b44b67363
Nicolas <nicolasgfalabella@gmail.com>
2024-02-16 12:19:07 -0300

    Intro a dict

---
+++ brais/07_diccionario.py
@@ -0,0 +1,19 @@
+""" Objetos"""
+my_dict = dict()
+
+my_other_dict= {}
+
+print(type( my_dict ))
+print(type( my_other_dict ))
+
+my_other_dict = {"Nombre" : "Brais" , "Apellido" : "Moure" , "Edad" : 35 , 1 : "Python"}
+
+my_dict = {
+        "Nombre" : "Brais" , 
+        "Apellido" : "Moure" , 
+        "Edad" : 35 , 
+        "Lenguajes": {"Python" , "Swift" , "Kotlin"}
+    }
+
+print( my_other_dict )
+print(my_dict)
