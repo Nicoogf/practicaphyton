0ea0764b3dcc852d3e6050d64f732ba03200a9f5
Nicolas <nicolasgfalabella@gmail.com>
2024-02-16 12:00:30 -0300

    Union y diference en sets

--- brais/06_sets.py
+++ brais/06_sets.py
@@ -33,4 +33,16 @@ prueba = my_other_set.copy()
 
 del my_other_set
 
-print(my_other_set)
+my_set = { "brais" , "Moure" , 35 }
+
+my_list = list(my_set)
+print(my_list)
+
+
+my_other_set = { "kotlin" , "Swift" , "Python" }
+
+my_new_set = my_set.union(my_other_set)
+
+print(my_new_set.union(my_new_set).union({"Javascript" , "C#"}))
+
+print(my_new_set.difference(my_set))
