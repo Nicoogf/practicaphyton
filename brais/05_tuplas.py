124c1a769bf2307fd079cb1b15f2a4b47ebfec32
Nicolas <nicolasgfalabella@gmail.com>
2024-02-16 10:45:04 -0300

    Tuplas 2

--- brais/05_tuplas.py
+++ brais/05_tuplas.py
@@ -5,6 +5,8 @@ my_tuple = tuple()
 my_other_tuple = ()
 
 my_tuple = (35,1.77,"Brais","Moure","Brais")
+my_other_tuple=(35,60,20)
+my_sum_tuple = my_tuple + my_other_tuple
 
 print(my_tuple)
 print(type(my_tuple))
@@ -13,3 +15,9 @@ print(my_tuple[0])
 print(my_tuple.count("Brais"))
 print(my_tuple.index("Moure"))
 print(my_tuple.index("Brais"))
+
+print(my_sum_tuple[3:6])
+
+my_tuple = list(my_tuple)
+
+print(type(my_tuple))
