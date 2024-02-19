65b73274ecc86581f45de7f62845d96eae048768
Nicolas <nicolasgfalabella@gmail.com>
2024-02-19 10:34:07 -0300

    Funcion con print y return

--- brais/10_funciones.py
+++ brais/10_funciones.py
@@ -7,8 +7,16 @@ my_function()
 my_function()
 my_function()
 
-def sum_two_values ( first_number:int , second_number:int) :
-    print(first_number + second_number)
+def sum_two_values ( first_value:int , second_value:int) :
+    print(first_value + second_value)
 
 sum_two_values( 5, 7 )
-sum_two_values( "5" , "7" )
+sum_two_values( 6 , 4.2 )
+
+
+def sum_two_values_with_return ( first_value:int , second_value:int) :
+    return first_value + second_value
+
+resultado = sum_two_values_with_return(10 , 5 )
+
+print(resultado)
