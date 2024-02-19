d0b4e4f6b891e12b40a16d969c7f4e113365a9b5
Nicolas <nicolasgfalabella@gmail.com>
2024-02-19 10:19:13 -0300

    Funciones con tipado

--- brais/10_funciones.py
+++ brais/10_funciones.py
@@ -1,3 +1,14 @@
 ### Funciones ###
 
-def 
+def my_function () :
+    print("Esto es una funcion")
+
+my_function()
+my_function()
+my_function()
+
+def sum_two_values ( first_number:int , second_number:int) :
+    print(first_number + second_number)
+
+sum_two_values( 5, 7 )
+sum_two_values( "5" , "7" )
