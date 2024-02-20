6ae65ec4cd473157f1e44272d6384f84ab2183d4
Nicolas <nicolasgfalabella@gmail.com>
2024-02-19 21:45:35 -0300

    Manejo de errores

--- brais/12_exepciones.py
+++ brais/12_exepciones.py
@@ -1,1 +1,15 @@
 """Exepciones"""
+
+numeroUno = 5
+numeroDos = 4
+numeroDos = "4"
+
+try:
+    print(numeroUno + numeroDos)
+    print("No se produjo error")
+
+except ValueError:
+    print("Se ha producido un error ValueError")
+except TypeError as error :
+    print(f"Se ha producido un error TypeError {error}")
+
