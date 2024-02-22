40253f14da7935a0e1d6026f087a58494440b2d7
Nicolas <nicolasgfalabella@gmail.com>
2024-02-22 07:42:07 -0300

    clousers

--- intermedio/03_fos.py
+++ intermedio/03_fos.py
@@ -15,3 +15,13 @@ def sum_two_funcion (valor_uno , valor_dos , funcion ) :
 
 print(sum_two_values_and_one(5,5))
 print(sum_two_funcion(5,2,sum_five))
+
+
+def sum_ten(original_value):
+    def add(value):
+        return value +10 + original_value
+    return add
+
+add_closure = sum_ten(1)
+print(add_closure(5))
+print(sum_ten(5)(1))
