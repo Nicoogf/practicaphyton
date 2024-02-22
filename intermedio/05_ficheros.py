7006e1bb6f18763d53a4f1002da6814f9a6c8238
Nicolas <nicolasgfalabella@gmail.com>
2024-02-22 10:29:53 -0300

    Creacion de txt

--- intermedio/05_ficheros.py
+++ intermedio/05_ficheros.py
@@ -2,5 +2,14 @@
 
 import os
 
-txt_file = open("intermedio/my_file.txt" , "w")
-txt_file.
+txt_file = open("./my_file.txt", "w+")
+
+txt_file.write("Creacion del archivo base")
+
+print(txt_file.readline())
+
+txt_file.write("\nNueva Linea de comando")
+
+txt_file.close()
+
+# os.remove("./my_file.txt")
