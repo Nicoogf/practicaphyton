06fcf197cecca7ff98d26252ff5f451ddd725d27
Nicolas <nicolasgfalabella@gmail.com>
2024-02-22 08:51:54 -0300

    Errores Segunda Parte

--- intermedio/04_errores.py
+++ intermedio/04_errores.py
@@ -5,3 +5,24 @@
 print ("Hola Comunidad")
 
 #NameErrors
+languaje = "spanish"
+print( languaje )
+
+
+#indexError 
+my_list = ["Python" , "Swift" , "Kotlin" , "Dart" , "Javascript"]
+# print(my_list[5])
+
+
+#ModulenotFound Errors
+
+# import maths
+import math
+
+
+#AtributteError
+
+# print(math.PI)
+print(math.pi)
+
+#keyError
