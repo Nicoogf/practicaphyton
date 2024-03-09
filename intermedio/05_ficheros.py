d136891a3735e2dd97387282b604cb21168aba75
Nicolas <nicolasgfalabella@gmail.com>
2024-03-09 17:13:27 -0300

    Add

--- intermedio/05_ficheros.py
+++ intermedio/05_ficheros.py
@@ -1,5 +1,6 @@
 """Ficheros"""
 
+import json
 import os
 
 txt_file = open("./my_file.txt", "w+")
@@ -13,3 +14,27 @@ txt_file.write("\nNueva Linea de comando")
 txt_file.close()
 
 # os.remove("./my_file.txt")
+
+json_file = open("./mi_practica.json", "w+")
+
+json_text = {
+    "Nombre": "Brais",
+    "Apellido": "Moure",
+    "Edad": 35,
+    "Lenguajes": ["Python", "Swift", "Kotlin"],
+    "web": "https://google.com"
+}
+
+json.dump(json_text, json_file)
+
+json_file.close()
+
+with open("./mi_practica.json") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+json_dict = json.load(open("./mi_practica.json"))
+
+print(json_dict)
+
+print(type(json_dict))
